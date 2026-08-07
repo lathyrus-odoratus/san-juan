1eb7fe1f903d64938ed8e64e8c4d6e6be46672c7
Shao <oxfoxlion@gmail.com>
2026-08-07 23:53:29 +0800

    feat:補充 lobby room 頁面要有哪些內容

--- app/pages/room/[id].vue
+++ app/pages/room/[id].vue
@@ -7,5 +7,8 @@ const roomId = computed(() => route.params.id as string)
 <template>
   <main>
     <h1>Room {{ roomId }}</h1>
+    <p>玩家格（4 格，含房主標示）</p>
+   <p>準備 / 開始遊戲按鈕</p>
+   <p>聊天室（Socket.io `client:send_message` / `server:chat_message`）</p>
   </main>
 </template>
